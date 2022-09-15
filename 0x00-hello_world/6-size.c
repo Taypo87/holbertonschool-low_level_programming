2bb9fe24be65e70eb3e1c7633ac11b8b522c8789
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-15 15:08:07 -0700

    try this

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ float f;
 int main(void)
 {
 
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu. \n", (unsigned long) sizeof(i));
-printf("Size of a long int: %lu. \n", (unsigned long)sizeof(x));
-printf("Size of a long long int %lu. \n", (unsigned long)sizeof(y));
-printf("Size of a float: %lu. \n", (unsigned long)sizeof(f));
+printf("Size of a char: %d.\n", (unsigned long)sizeof(c));
+printf("Size of an int: %d.\n", (unsigned long) sizeof(i));
+printf("Size of a long int: %d.\n", (unsigned long)sizeof(x));
+printf("Size of a long long int %d.\n", (unsigned long)sizeof(y));
+printf("Size of a float: %d.\n", (unsigned long)sizeof(f));
  return(0);
 }
