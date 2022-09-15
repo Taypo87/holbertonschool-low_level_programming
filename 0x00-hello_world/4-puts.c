04ecc26dd26fe413c2486eb79c7561499229afec
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-15 09:05:06 -0700

    fix for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
+/* this will print a statement using puts */
 
 #include <stdio.h>
 
 int main()
 
 {
-  puts("Programming is like building a multilingual puzzle")
+  puts("Programming is like building a multilingual puzzle");
  return 0
 }
