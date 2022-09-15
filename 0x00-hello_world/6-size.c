e909dde005348be97fb5bb4c52705a396e0c82d6
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-15 13:57:36 -0700

    1st attempt at 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+/*
+ *  File: 6-size.c
+ *  Author: Taylor Poczynek
+ */
+#include <studio.h>
+
+char c;
+int i;
+long int x;
+long long int y;
+float f;
+
+int main(void)
+{
+
+printf("Size of a char: %lu.\n", (unsigned long)size of(c));
+printf("Size of an int: %lu. \n", (unsigned long) size of(i));
+printf("Size of a long int: %lu. \n", (unsigned long)size of(x));
+printf("Size of a long long int %lu. \n", (unsigned long)size of(y));
+printf("Size of a float: %lu. \n", (unsigned long)size of(f));
+return(0)
+}
