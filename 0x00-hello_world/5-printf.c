c789a28b1fa3cf336fe70e0a92705ff739a8b692
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-15 11:29:01 -0700

    1st attempt at 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ *File: 5-printf.c
+ *Auth: Taylor Poczynek
+ */
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammer, but the outcome is a piece of art,"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,");
+return(0);
+}
