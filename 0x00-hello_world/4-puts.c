76d8d3733a4471b0a0e26d91e2e4bc6fafeaccaf
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-15 09:34:02 -0700

    fix for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 /* this will print a statement using puts */
 
 #include <stdio.h>
-/* this will use puts to print a statement */
+/**
+* this will use puts to print a statement
+*
+*/
 int main(void)
-
+/**
+*
+*
+*/
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
