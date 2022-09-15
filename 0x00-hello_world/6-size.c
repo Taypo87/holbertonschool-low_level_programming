f9feca1b2275502b33dd8aa4651af05bca8643bc
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-15 15:13:14 -0700

    try this

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %d byte(s)\n", (unsigned long) sizeof(i));
 printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long long int %d byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a float: %d byte (s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
  return(0);
 }
