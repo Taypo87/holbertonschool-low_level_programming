e4612c4232e30c56ea7765839e6f8de18620b9b6
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-18 07:45:33 -0700

    1st attempt at 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/*
+*  File - 2-print_alphabet
+*  Author: Taylor Poczynek
+*/
+#include <stdio.h>
+/**
+* main - prints alphabet
+* Return: 0
+*/
+int main(void)
+{
+for (int c = 'a'; c <= 'a'; c++)
+putchar(c);
+puchchar("\n")
+return (0);
+}
