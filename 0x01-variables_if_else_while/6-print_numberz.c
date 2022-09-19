adf987b58b491b8423a15b1eed399af2fdfb485d
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-19 09:32:50 -0700

    1st attempt at 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/*
+ * File - 6-print_numbers.c
+ * Author: Taylor Poczynek
+ */
+#include <stdio.h>
+/**
+ * main - prints 0-9
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for
+( a = 0;
+a < 10;
+a++)
+{
+putchar(a)
+}
+putchar('\n')
+return (0);
+}
