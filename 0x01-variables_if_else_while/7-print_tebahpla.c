645a5a863429fa02bae414d5b6794ec3654bef04
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-19 10:59:49 -0700

    1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ */
+#include <stdio.h>
+/**
+ * main - prints alphabet in
+ * Return: 0
+ */
+int main(void)
+{
+char x;
+x = 'z';
+while (x >= 'a')
+{putchar (x);
+(x--);
+}
+putchar ('\n');
+return (0);
+}
