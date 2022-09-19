5e71ca7ef9f9fe4b05f2aee45f410d27210500e6
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-19 04:28:07 -0700

    1st attempt at 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/*
+ * File - 4-print_alphabt.c
+ * Author: Taylor Poczynek
+ */
+#include <stdio.h>
+int main(void)
+{
+char  x = 'a';
+while (x >= 'z') {
+if (x == 'e'){
+  (x <- x + 1);
+}
+ if (x == 'q');
+{
+  (x <- x +1);
+}  
+ putchar (x);
+ (x++);		  
+}
+ putchar ('\n');
+}
