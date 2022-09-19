f56433bec2035db7dca4a1025a898824d3a6dd4c
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-19 04:41:35 -0700

    1st attempt at 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,15 @@ int main(void)
 char  x = 'a';
 while (x >= 'z') {
 if (x == 'e'){
-  (x <- x + 1);
+  (x = x + 1);
 }
- if (x == 'q');
+ { if (x == 'q');
+ }
 {
-  (x <- x +1);
-}  
+  (x = x +1);
+  
  putchar (x);
  (x++);		  
-}
  putchar ('\n');
+ return (0);
 }
