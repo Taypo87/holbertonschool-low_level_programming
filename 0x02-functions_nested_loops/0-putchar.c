2e863823b33c6564a0d7e3612d3b14252f51ce5c
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-20 03:45:15 -0700

    fix for 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,15 @@
 */
 int main(void)
 {
-  putchar('-')
-    putchar('p')
-    putchar('u')
-    putchar('t')
-    putchar('c')
-    putchar('h')
-    putchar('a')
-    putchar('r')
-    putchar('\n')
+  putchar('-');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
     }
 
 return(0)
