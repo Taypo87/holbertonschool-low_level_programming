ce41bec3e1a9ab10a7a691552ee8388516c7e7e9
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-20 07:10:58 -0700

    fix for 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,16 @@
 *  Author: Taylor Poczynek
 */
 /**
-*  main - writes the alphabet using prototypes
+*  print_alphabet - writes the alphabet using prototypes
 *  Return: 0
 */
 void print_alphabet(void)
 {
-char c; 
+char c;
 for
-(c = 'a'; c <= 'z'; ++c)
+(c = 'a';
+c <= 'z';
+++c)
 {
 _putchar(c);
 }
