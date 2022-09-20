a1354cbf26ea31d69bd5aac10cee3cecfe3c5c51
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-20 07:46:40 -0700

    1st attempt at 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 prints the alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int x = '0';
+char a = 'a';
+while (x > 10)
+{
+while (a <= 'z')
+{
+_putchar(a);
+(a++);
+}
+_putchar('\n');
+(x++);
+}
+}
