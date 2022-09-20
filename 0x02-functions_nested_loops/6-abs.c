31a7a884a38bf2ca8fd5ce74728810fd7bb7e125
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-20 15:11:26 -0700

    1st attempt at 5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - checks absolute value
+ * @n: variable
+ * Description: returns the absolute value of a number
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
