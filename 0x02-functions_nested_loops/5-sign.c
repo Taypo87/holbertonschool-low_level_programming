41500fab8bd508678dcfc23d41284243b71c95b0
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-20 14:30:13 -0700

    1st attempt at 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,21 @@
 /**
  * print_sign - prints the sign of a number
  * @n: variable
- * Description: prints + - or 0 
+ * Description: prints + - or 0
  * Return: -1 if negative 0 if 0 and 1 if positive
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-	        return (1);
-	}
-      	else if (n < 0)
-	{
-		_putchar('-');
-	        return (-1);
-	}
-	_putchar('0');
-	return (0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+_putchar('0');
+return (0);
 }
