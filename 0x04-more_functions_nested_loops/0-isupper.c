e9ec8f84f1842647da7a7930a37f9777e864d6ae
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-22 07:40:25 -0700

    1st attempt at 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * _isupper - checks if letter is lower case
+ * @c: variable
+ * Description: returns 1 or 0
+ * Return: 1 if lower 0 if not
  */
 int _isupper(int c)
 {
