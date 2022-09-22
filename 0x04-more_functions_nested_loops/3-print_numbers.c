8b9553b7019699d04bfe4428ac6569f1e4e21f3f
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-22 10:46:07 -0700

    fixin stuff

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0-9
+ * @i carrier variable
+ * Description: prints 0-9
+ * Return: 0
+ *
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	for (i = 0; 1 < 10; i++)
+		_putchar (i);
+_putchar('\n');
+}
