c00c4c8a757aa9ec3c32bba28c204fe5d6d7bdff
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-22 09:36:34 -0700

    1st attempt at 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two number
+ * @a: is a carrier variable
+ * @b: is a carrier variable
+ * Description: multiplies two numbers
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
