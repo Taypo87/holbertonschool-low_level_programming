7571406e9b101df7cf40fe0636586d5c5b986b07
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-22 12:40:07 -0700

    working on 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{	int i = 0;
+	if (i > 10 && i != 2 && i != 4)
+	{
+		_putchar (i);
+			i++;
+	}
+
+	else if (i == 2 || i == 4)
+		{
+		i++;
+		}
+
+}
