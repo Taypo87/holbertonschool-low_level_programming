f539992d705c51863864d59f26ac957ba4eddba4
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-23 09:25:15 -0700

    working on 7 and 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_square(int size)
+{
+	int count = size
+		while (count != 0)
+		{
+			_putchar('#')
+			if (count = 0)
+			{
+				_putchar('\n')
+			}
+		}
+}
