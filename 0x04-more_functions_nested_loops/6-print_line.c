6dc64e28b9d17889e75f39ba1226a185aa722bf3
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-23 03:58:04 -0700

    1st attempt at 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * print_line - prints a line
+ * @n: defines how long the line is
+ * Description: draws a line, length dependant on variable
+ * Return: void
  */
 void print_line(int n)
+{
 	if (n > 0)
-		_putchar("_");
+	{
+		_putchar('_');
 			n--;
-			else
-				_putchar('\n')
+	}
+	else
+	{
+				_putchar('\n');
+
+	}
+}
