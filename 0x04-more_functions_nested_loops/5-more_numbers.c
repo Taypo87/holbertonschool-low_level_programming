12b919eb26a321e2206c94025b6eb2b2f305de1e
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-23 07:27:26 -0700

    fix for 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,10 @@ void more_numbers(void)
 	int x = 0;
 	int y;
 
-		while (x > 10)
+		while (x < 10)
 			  {
 				  y = 0;
-				  while (y > 15)
+				  while (y < 15)
 				  {
 					  _putchar((y / 10) + '0');
 					  _putchar((y % 10) + '0');
