34043c14770c279997fa3ed5234750d351493dd3
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-24 13:25:31 -0700

    8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,19 @@
 #include "main.h"
-/**
- *
- *
- *
- *
- */
+
 void print_diagonal(int n)
+{
+	int y = n;
+	int x = 0;
+	while (y > 0)
+	{
+		_putchar('\\');
+		_putchar('\n');
+			 y--;
+			 while (x < n)
+			 {
+				 _putchar(' ');
+					 x++;
+			 }
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,25 @@
 #include "main.h"
+
 /**
- *
- *
- *
- *
- */
+* print_square - creates a square
+* @size: variable dictaes size of square
+*
+*
+*/
 void print_square(int size)
 {
-	int count = size
-		while (count != 0)
-		{
-			_putchar('#')
-			if (count = 0)
-			{
-				_putchar('\n')
-			}
-		}
+int row = size;
+int count;
+
+while (row > 0)
+{
+row--;
+count = size;
+while (count > 0)
+{
+_putchar('#');
+count--;
+}
+_putchar('\n');
+}
 }
