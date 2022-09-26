b7823ac272d5b8cee31a1b95f393ec9574ed655a
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-26 07:25:23 -0700

    first attempt at 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * swap_int - swaps the value of a and b
- * a: integar value to swap
- * b: integar value to swap
+ * @a: integar value to swap
+ * @b: integar value to swap
  */
 
 void swap_int(int *a, int *b)
