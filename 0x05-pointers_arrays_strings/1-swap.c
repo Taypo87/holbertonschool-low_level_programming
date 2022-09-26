ee9eea5e965dbf701bcab9e7e3cfc43edb51f107
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-26 07:23:36 -0700

    first attempt at 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of a and b
+ * a: integar value to swap
+ * b: integar value to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
