940259c0645a8f2c4d1e39a3c1bda7b10dede715
Taypo87 <taylor.poczynek87@gmail.com>
2022-09-26 09:18:20 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: variable contains string to be written
+ *
+ */
+void print_rev(char *s)
+{
+	int length = 0; index;
+
+	while (s[index]++)
+		length++;
+	for (index = len -1; index >= 0; index--)
+		_putchar(s[index]);
+	_putchar('\n');
+}
