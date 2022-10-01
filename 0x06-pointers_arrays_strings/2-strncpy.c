5aac47bee9bf4eef473844a1d2458bc4b83c8533
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-01 14:18:47 -0700

    1st attempt at 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * _strncpy - copies a string
+ * @dest: destination variable to hold copied string
+ * @src: string to be copied
+ * @n:  variable
+ * Return: copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
