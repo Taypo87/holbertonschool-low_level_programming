a5f6937aab75a55e8c69030a717df6d9f8ec80bb
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-01 14:25:18 -0700

    1st attempt at 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,8 +5,11 @@
  * @src:string to be copied
  * Return: copied string
  */
-{
 
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i = 0;
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
