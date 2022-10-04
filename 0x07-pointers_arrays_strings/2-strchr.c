0b8d01bafa9ada100882d2b7198ac2e9147b76bb
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-04 04:43:44 -0700

    commit for 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates first occurance of a character
+ * @s: string to be checked
+ * @c: character to be located
+ * Return: address of first occurance or null
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0
+
+while s[i]
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
+}
