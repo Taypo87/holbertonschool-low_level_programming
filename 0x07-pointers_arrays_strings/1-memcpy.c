59bf3847a276f5587e9f7aa211095e85b76fa56d
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-04 04:07:34 -0700

    first attempt at 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _memcpy - copies one chunk of memory to another
+ * @dest: to be copied to
+ * @src: to be copied
+ * @n: how many bytes to be copied
+ * Return: copied memory
  */
 
-har *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	
+	for (i = 0; i < n; i++)
+	{
+
+		(dest[i]) = (src[i]);
+
+	}
+	return (dest);
+}
