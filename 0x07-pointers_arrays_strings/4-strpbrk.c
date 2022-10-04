c07031da96f2eb20c09dc0355bc35cae3a024d06
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-04 12:54:34 -0700

    fix for 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,9 +16,8 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
-	{
-		return (&s[i]);
-	}
+				return (&s[i]);
+		}
 			s++;
 	}
 	return (NULL);
