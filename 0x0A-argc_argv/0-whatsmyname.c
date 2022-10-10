1e607fc842bf2bf36a16f1fb02ecdbd1eac501f7
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-10 08:06:33 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,12 @@
  * main - prints program name followed by a new line
  * @argc: argument count
  * @argv: arguments given to the program
+ * Return: 0
  */
 
-int main(int __attribute__((__unused__)) argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-		return(0);
+		return (0);
 }
