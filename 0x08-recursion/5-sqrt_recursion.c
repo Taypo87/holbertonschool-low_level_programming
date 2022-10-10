14c0c042adb37c4f48e1c2953ea8b4678ebd7cf1
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-10 08:02:06 -0700

    0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: number to test
+ * Return: -1 if no natural square root found, or the result
+ */
+
+int _sqrt_recursion(int n)
+{
+	
