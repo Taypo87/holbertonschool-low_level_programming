268737464f02e37a365713d4f40780dc223c69a9
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-10 08:15:34 -0700

    0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the number of arguemnts given
+ * @argc: count of arguments
+ * @argv: contains the arguments passed
+ * Return: 0
+ */
+
+int main(int argc,char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
