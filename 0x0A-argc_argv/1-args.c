87f139df24dc84dfe25e476bfcf5abdaa273fee9
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-10 08:19:34 -0700

    0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * Return: 0
  */
 
-int main(int argc,char __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", (argc - 1));
 
 	return (0);
 }
