ed9cdbe139d8975ab63afe7f1cc677237d45ad2f
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-10 11:39:19 -0700

    fix for 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the sum of positive numbers
+ * @argc: count of arguments passed
+ * @argv: array pointer to arguements passed
+ * Return: 1 for error or 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
