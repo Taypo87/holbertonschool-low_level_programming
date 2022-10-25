fa294e5b84ad6c07e7ce6c03c724ef9b773b79a1
Taypo87 <taylor.poczynek87@gmail.com>
2022-10-25 04:37:13 -0700

    update

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * sum_them_all - sums up a variable number of ints
+ * @n: number of args passed
+ * Return: sum of ints
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = 0;
+	int sum = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	while (i < n)
+	{
+		sum += va_arg(ap, int);
+		i++;
+	}
+
+	va_end(ap);
+	return (sum);
+}
