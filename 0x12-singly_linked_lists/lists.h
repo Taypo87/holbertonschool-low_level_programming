cc462d7fa1f9cb6ff89c7c62f247d05a00b704f2
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-06 12:39:59 -0800

    fixd lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 
 typedef struct list_s
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of a string
+ * @next: pointer to the next node
+ */
 {
 	char *str;
 	unsigned int len;
