54997b718681b44352b19651d63f658c6864c1bd
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-07 06:49:46 -0800

    first attempt at 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,16 @@
 void free_listint2(listint_t **head)
 
 {
-	listint_t tmp;
+	listint_t *tmp;
 
 	if (head == NULL)
 		return;
 
-	while (head)
+	while (*head)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
 
 	head = NULL;
