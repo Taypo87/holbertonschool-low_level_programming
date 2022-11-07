754dadc2c0607e61cc3ac8e21292269da2948b9c
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-07 04:07:17 -0800

    fix for 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,13 +2,13 @@
 
 /**
  * print_listint - iterates through and prints each nodes content
- * @h: the list to print
+ * @h: a pointer to the list to print
  * Return: number of nodes in the list
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0
+	size_t count = 0;
 
 	while (h)
 	{
