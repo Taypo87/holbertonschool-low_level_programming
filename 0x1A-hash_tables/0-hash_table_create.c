59bc3acb3659c0d8b2d11f0516db400374853881
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-10 09:33:20 -0800

    first attempt at 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table of hash_table_t type
+ * @size: size of the array of in hash table
+ * Return: pointer the hash table or null on error
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (table->array == NULL)
+		return(NULL);
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
