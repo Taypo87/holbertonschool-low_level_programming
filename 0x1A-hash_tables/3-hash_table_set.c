340081c549b36732e1d1ecb455497708e3f535f3
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-14 09:16:35 -0800

    undid changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,12 +18,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (strcmp(ht->array[index]->key, key) == 0)
-		{
-			ht->array[index]->value = strdup(value);
-			return (1);
-		}
-
 	new = malloc(sizeof(hash_node_t));
 
 	if (new == NULL)
