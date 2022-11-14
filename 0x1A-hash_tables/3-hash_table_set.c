192813d4d731468b9644d54ae1d1f7e77571ec2a
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-14 08:55:10 -0800

    first attempt at 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,4 +8,23 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+	hash_node_t new;
+	unsigned int index;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->[index];
+	ht->[index] = new;
+
+	return (1);
+}
