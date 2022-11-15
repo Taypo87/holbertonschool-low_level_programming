37965d595020423dece7150a128bf5b04677ab52
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-15 04:13:42 -0800

    second attempt at 2

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -16,16 +16,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (head == NULL)
-		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = head;
-	head = new;
-
-	h = h->next;
-	h->prev = head;
+	new->next = *head;
+	if ((*head) != NULL)
+		(*head)->prev = new;
+	*head = new;
 
 	return (new);
 }
