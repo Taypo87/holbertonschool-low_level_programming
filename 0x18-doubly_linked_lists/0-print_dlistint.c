400cf37855a964bed4201a867986d01077d741b4
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-15 03:02:48 -0800

    header file and project 0

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include lists.h
+
+/**
+ * print_dlistint - prints a given list to standard out
+ * @h: the list to print
+ * Return: number of nodes printed
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+		printf("%i", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x18-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+readme for 0x18-doubly_linked_lists
