19a105d258418d46922d8e295e528f2f721f51d0
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-15 06:17:27 -0800

    first attempt at 6

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - adds the values stored in each node
+ * @head: pointer to the first node the the list
+ * Return: the sum of all values in the list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+	int sum = 0;
+
+	node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return(0);
+	node = head;
+
+	while (node)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+	return (sum);
+}
