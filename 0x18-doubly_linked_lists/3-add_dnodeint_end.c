e20c318bf44a77a855c1ef0e2a0957a163983c2e
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-15 05:13:20 -0800

    fixed betty

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -27,7 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	last = *head;
 
-	while (last->next != NULL )
+	while (last->next != NULL)
 		last = last->next;
 
 	last->next = new;
