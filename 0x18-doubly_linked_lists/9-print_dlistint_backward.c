c1ecd283047f9c0de3c04627120cc59483266653
Taypo87 <taylor.poczynek87@gmail.com>
2022-11-16 09:03:05 -0800

    fixed tail

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - prints a list starting with the last node
+ * @h:pointer to the list to print
+ * Return: the number of nodes printed
+ */
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	const dlistint_t *node, *tail;
+	size_t count;
+
+	node = h;
+
+	while (node)
+	{
+		tail = node;
+		node = node->next;
+		count++;
+	}
+
+	while (tail)
+	{
+		printf("%i", tail->n);
+		tail = tail->prev;
+	}
+	return (count);
+}
